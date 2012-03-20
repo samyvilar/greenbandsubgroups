92cf1ef556c337ac25d13682ef4b5476d634b5c5
Samy Vilar <samy.vilar@gmail.com>
2012-03-20 00:06:56 -0400

    disbaled the caching for testing ...

--- runs/4_groups_4_subgroups/loading_granules/load.py
+++ runs/4_groups_4_subgroups/loading_granules/load.py
@@ -9,7 +9,7 @@ import numpy
 granule_loader = GranuleLoader()
 granule_loader.bands = numpy.asarray([1,2,3,4])
 granule_loader.param = 'reflectance'
-granule_loader.enable_caching()
+granule_loader.disable_caching()
 granule_loader.enable_multithreading()
 granule_loader.load_granules_from_dir(dir = '/home1/FoucaultData/DATA_11/TERRA_1KM', pattern = '*.hdf')
 
