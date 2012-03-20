5c5a0027e9e150cba5a90c3b1b566582aeac07b5
Samy Vilar <samy.vilar@gmail.com>
2012-03-20 18:43:20 -0400

    Testing lut builder ...

--- runs/lookuptable/reflectance/600/build.py
+++ runs/lookuptable/reflectance/600/build.py
@@ -9,30 +9,35 @@ from lookuptable.lookuptable import lookuptable, build_lookuptable
 from GranuleLoader import GranuleLoader
 from Utils import multithreading_pool_map
 
-#if __name__ == '__main__':
-lut = lookuptable()
-granule_loader = GranuleLoader()
-granule_loader.bands = [1,2,3,4]
-granule_loader.param = 'reflectance'
-granule_loader.disable_caching()
-granule_loader.enable_multithreading()
+if __name__ == '__main__':
+    granule_loader = GranuleLoader()
+    granule_loader.bands = [1,2,3,4]
+    granule_loader.param = 'reflectance'
+    granule_loader.disable_caching()
+    granule_loader.enable_multithreading()
 
-granule_loader_chunks = granule_loader.load_granules_chunk(dir = '/home1/FoucaultData/DATA_11/TERRA_1KM', pattern = '*.hdf', chunks = 1)
-lut_size = 5000
+    granule_loader_chunks = granule_loader.load_granules_chunk(dir = '/home1/FoucaultData/DATA_11/TERRA_1KM', pattern = '*.hdf', chunks = 1)
+    lut_size = 600
 
-lut = build_lookuptable({'data':granule_loader_chunks.next()[0].data, 'size':lut_size})
+    all_avg_lut = build_lookuptable({'data':granule_loader_chunks.next()[0].data, 'size':lut_size})
 
-gc.collect()
+    gc.collect()
 
-granule = granule_loader_chunks.next()[0]
-temp_lut = build_lookuptable({'data':granule[0].data, 'size':lut_size})
-prev_counts = numpy.copy(lut.counts)
-lut.counts += temp_lut.counts
-non_zero_locations = lut.counts != 0
-lut.table = (temp_lut.sums[non_zero_locations]/lut.counts[non_zero_locations]) + (prev_counts * lut.table)/lut.counts[non_zero_locations]
-lut.table[non_zero_locations] /= lut.table[lut.counts != 0]
+    for granule in granule_loader_chunks.next():
+        new_lut = build_lookuptable({'data':granule[0].data, 'size':lut_size})
 
-#tables = multithreading_pool_map(function = build_lookuptable, values = [{'data':c.data, 'size':600} for c in chunk], multithreaded = True)
+        prev_sum_counts = numpy.copy(all_avg_lut.counts)
+        all_avg_lut.counts += new_lut.counts
+        non_zero_locations = all_avg_lut.counts != 0
+        all_avg_lut.table = (new_lut.sums[non_zero_locations]/all_avg_lut.counts[non_zero_locations]) + \
+                    ((prev_sum_counts[non_zero_locations] * lut.table[non_zero_locations])/lut.counts[non_zero_locations])
+        del new_lut
+        del prev_sum_counts
+        gc.collect()
+
+    all_avg_lut.table.tofile(str(lut_size) + '_lookuptable.numpy')
+    all_avg_lut.counts.tofile(str(lut_size) + '_size.numpy')
+
 
 
 
