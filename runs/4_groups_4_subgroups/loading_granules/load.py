48035646e5b88b8543bc236e775474b3a8df8230
Samy Vilar <samy.vilar@gmail.com>
2012-03-20 03:58:47 -0400

    minor bug fixes ...

--- runs/4_groups_4_subgroups/loading_granules/load.py
+++ runs/4_groups_4_subgroups/loading_granules/load.py
@@ -9,7 +9,7 @@ import numpy
 granule_loader = GranuleLoader()
 granule_loader.bands = [1,2,3,4]
 granule_loader.param = 'reflectance'
-granule_loader.enable_caching()
+granule_loader.disable_caching()
 granule_loader.enable_multithreading()
 granule_loader.load_granules_from_dir(dir = '/DATA_11/TERRA_1KM', pattern = '*.hdf')
 
