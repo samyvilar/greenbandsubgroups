c6fe3be25b50782d86c11d42b22159c1ffa7a36a
Samy Vilar <samy.vilar@gmail.com>
2012-03-20 14:44:10 -0400

    added a simple build script for c library liblut
    --HG--
    rename : lookuptable/build_lib.py => lookuptable/makefile

--- lookuptable/build_lib.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Samy Vilar'
-__date__ = 'Mar 20, 2012'
-
-import subprocess
-
-
-if __name__ == '__main__':
-    subprocess.call('/usr/bin/gcc -Wall -fPIC -c *.c && /usr/bin/gcc -shared -Wl,-soname,liblut.so -o liblut.so  *.o')
-
