292ddc940ade9e1e22a312d8aed1c2e1e7927fa5
Samy Vilar <samy.vilar@gmail.com>
2012-03-19 23:02:46 -0400

    Trying the home1 path instead of DATA_1

--- runs/4_groups_4_subgroups/loading_granules/load.py
+++ runs/4_groups_4_subgroups/loading_granules/load.py
@@ -10,6 +10,6 @@ granule_loader = GranuleLoader()
 granule_loader.bands = numpy.asarray([1,2,3,4])
 granule_loader.param = 'reflectance'
 granule_loader.disable_caching()
-granule_loader.disable_multithreading()
-granule_loader.load_granules_from_dir(dir = '/DATA_11/TERRA_1KM', pattern = '*.hdf')
+granule_loader.enable_multithreading()
+granule_loader.load_granules_from_dir(dir = '/home1/FoucaultData/DATA_11/TERRA_1KM', pattern = '*.hdf')
 
