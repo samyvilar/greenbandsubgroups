a24e6ec61fafc2d25714404f192cb62417416c75
Samy Vilar <samy.vilar@gmail.com>
2012-03-20 12:57:20 -0400

    minor bug fixes ...

--- runs/lookuptable/reflectance/600/build.py
+++ runs/lookuptable/reflectance/600/build.py
@@ -14,7 +14,7 @@ granule_loader.disable_caching()
 granule_loader.enable_multithreading()
 
 chunk = granule_loader.load_granules_chunk(dir = '/DATA_11/TERRA_1KM', pattern = '*.hdf', chunks = 10).next()
-t = build_lookuptable({'data':chunk[0].data, 'size':600})
+t = build_lookuptable({'data':chunk[0].data, 'size':1000})
 
 tables = multithreading_pool_map(function = build_lookuptable, values = [{'data':c.data, 'size':600} for c in chunk], multithreaded = True)
 
