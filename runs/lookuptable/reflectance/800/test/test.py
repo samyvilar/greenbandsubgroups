dad71ba1e339c164d6fc26ac8a12d99e130aedda
Samy Vilar <samy.vilar@gmail.com>
2012-03-22 02:06:17 -0400

    Testing the prediction of the lookuptable ...

--- runs/lookuptable/reflectance/800/test/test.py
+++ runs/lookuptable/reflectance/800/test/test.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     granule_loader.enable_multithreading()
 
     granule_loader.load_granules(granules = ['/home1/FoucaultData/DATA_11/TERRA_1KM/MOD021KM.A2010172.1200.005.2010172204449.hdf',])
-    original = granule_loader.granules[0].data[:, 3]
+    original = granule_loader.granules[0].data
     predicted = lut.predict(original)
 
     std = numpy.sqrt(numpy.sum((predicted[:, 3] - original[:, 3])**2)/original[:, 3].shape[0])
