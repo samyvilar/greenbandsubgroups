9481d9e7c43adc73ec375465f311c8bd00bed566
Samy Vilar <samy.vilar@gmail.com>
2012-03-22 20:18:03 -0400

    Minor bug to lookuptable c header file ..

--- lookuptable/lut.h
+++ lookuptable/lut.h
@@ -1,3 +1,3 @@
 void lookuptable(int *, unsigned int, unsigned int, float *, float *, unsigned int);
-void predict_double(int *, unsigned int, unsigned int, double, unsigned int, double);
+void predict_double(int *, unsigned int, unsigned int, double *, unsigned int, double *);
 
