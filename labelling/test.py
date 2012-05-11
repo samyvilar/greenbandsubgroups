d58488a39f1b61e60be61287c3ebe41cc12837dc
Samy Vilar <samy.vilar@gmail.com>
2012-05-11 04:35:12 -0400

    Made a more agressive test for the labelling and added timing information ...

--- labelling/test.py
+++ labelling/test.py
@@ -2,13 +2,25 @@ __author__ = 'samyvilar'
 
 from labelling import get_labels
 import numpy
+import time
+
+if __name__ == '__main__':
+    data = numpy.random.rand(10000, 4)
+    means = numpy.random.rand(4, 4)
 
 
-data = numpy.random.rand(1000, 4)
-means = numpy.random.rand(4, 4)
+    start = time.time()
+    dist = numpy.zeros((data.shape[0], means.shape[0]))
+    for i in xrange(means.shape[0]):
+        dist[:, i] = numpy.sum((data - means[i,:])**2, axis = 1)
+    labels = dist.argmin(axis = 1)
+    end = time.time()
+    print "Numpy time %f" % (end - start)
 
-dist = numpy.zeros((data.shape[0], means.shape[0]))
-for i in xrange(means.shape[0]):
-    dist[:, i] = numpy.sum((data - means[i,:])**2, axis = 1)
-labels = dist.argmin(axis = 1)
+    start = time.time()
+    labels2 = get_labels(data = data, means = means)
+    end = time.time()
+
+    assert all(labels == labels2)
+    print "OK"
 
