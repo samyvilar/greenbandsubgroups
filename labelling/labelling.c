db59518318f43b307829ea217eb2e82554d2bd46
Samy Vilar <samy.vilar@gmail.com>
2012-05-12 02:46:34 -0400

    Testing labelling for sub groups ...

--- labelling/labelling.c
+++ labelling/labelling.c
@@ -101,7 +101,7 @@ void set_labels(double *data, unsigned int data_number_of_rows, unsigned int dat
                         total_distances[index_2][index] += distances[index_2][index][index_1];
 
                     }
-                    printf("distances[%i][%i][%i] = %f\n", index_2,index,index_1,distances[index_2][index][index_1]);
+                    printf("total_distances[%i][%i] = %f\n", index_2,index,total_distances[index_2][index] );
                 }
         unsigned int **labelsp = (unsigned int **)malloc(data_number_of_rows * sizeof(unsigned int *));
         for (index = 0; index < data_number_of_rows; index++)
--- labelling/test.py
+++ labelling/test.py
@@ -3,7 +3,7 @@ __author__ = 'samyvilar'
 from labelling import get_labels
 import numpy
 import time
-import scipy.cluster.vq
+
 
 if __name__ == '__main__':
     data = numpy.random.rand(10000000, 4)
@@ -35,21 +35,21 @@ for mean_index, mean in enumerate(means):
         dist[:, mean_index, i] = numpy.sum((data - mean[i,:])**2, axis = 1)
 
 
-    labels = numpy.zeros((data.shape[0], 2), dtype = 'int')
-    labels[:, 0] = dist.sum(axis = 2).argmin(axis = 1)
-    for index in xrange(dist.shape[0]):
-        labels[index, 1] = dist[index][labels[index, 0]].argmin()
+labels = numpy.zeros((data.shape[0], 2), dtype = 'int')
+labels[:, 0] = dist.sum(axis = 2).argmin(axis = 1)
+for index in xrange(dist.shape[0]):
+    labels[index, 1] = dist[index][labels[index, 0]].argmin()
 
 
-    end = time.time()
-    print "2D Numpy time %f" % (end - start)
+end = time.time()
+print "2D Numpy time %f" % (end - start)
 
-    start = time.time()
-    labels2 = get_labels(data = data, means = means)
-    end = time.time()
-    print "2D time %f" % (end - start)
+start = time.time()
+labels2 = get_labels(data = data, means = means)
+end = time.time()
+print "2D time %f" % (end - start)
 
-    assert numpy.sum(labels - labels2) == 0
-    print "2D OK"
+assert numpy.sum(labels - labels2) == 0
+print "2D OK"
 
 
