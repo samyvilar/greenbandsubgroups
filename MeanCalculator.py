80fefc4b1f7657db697502ecab75a619bf4c99eb
Samy Vilar <samy.vilar@gmail.com>
2012-05-30 04:26:24 -0400

    minor bug ...

--- MeanCalculator.py
+++ MeanCalculator.py
@@ -63,7 +63,7 @@ def check_for_empty_groups(data = None, labels = None, means = None):
     assert data != None and labels != None and means != None
     if len(labels.shape) == 1:
         groups = xrange(means.shape[0])
-        empty_groups = numpy.asarray([str(group) in str(labels) for group in groups])
+        empty_groups = numpy.asarray([group in labels for group in groups])
     elif len(labels.shape) == 2:
         groups = [[group, subgroup] for group in xrange(means.shape[0]) for subgroup in xrange(means.shape[1])]
         empty_groups = [any(( (labels[:, 0] == group[0]) & (labels[:, 1] == group[1]) )) for group in groups]
