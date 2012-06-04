829d15031cfd3968e7a092a3493301bba5c052b8
Samy Vilar <samy.vilar@gmail.com>
2012-06-04 00:37:24 -0400

    ...

--- runs/4_groups_4_subgroups/initial_means/test_means.py
+++ runs/4_groups_4_subgroups/initial_means/test_means.py
@@ -4,42 +4,37 @@ __date__ = 'Mar 22, 2012'
 import sys
 sys.path.extend('../../..')
 
-import os.path
-import pickle
-
 import numpy
 import time
 from lookuptable.lookuptable import  lookuptable
-from MeanCalculator import MeanCalculator, minimize, get_predicted_from_means
-from GranuleLoader import GranuleLoader
-from Utils import save_images, get_root_mean_square, get_sum_of_errors_squared, get_granule_path
+from MeanCalculator import  minimize, get_predicted_from_means
+from Utils import get_sum_of_errors_squared,\
+    get_granule_path, get_standard_granule_loader, get_standard_mean_calculator,\
+    get_previous_means, save_optimal_solutions
 from matplotlib import pyplot as plt
+
 
 lut = lookuptable()
 lut.load_flatten_table('../../lookuptable/reflectance/800_non_capped')
 lut_data_flatten = lut.indices_to_data(lut.flatten_table)
 
-granule_loader = GranuleLoader()
-granule_loader.bands = [1,2,3,4]
-granule_loader.param = 'reflectance'
-granule_loader.disable_caching()
-granule_loader.disable_multithreading()
 granule_path = get_granule_path() + 'MOD021KM.A2002179.1640.005.2010085164818.hdf'
 
+granule_loader = get_standard_granule_loader()
 granule_loader.load_granules(granules = [granule_path,])
 original = granule_loader.granules[0].data
 
-mean_calculator = MeanCalculator()
-mean_calculator.enable_multithreading()
-mean_calculator.disable_caching()
-
-mean_calculator.threshold = 1e-06
-mean_calculator.number_of_groups = 2
-mean_calculator.number_of_sub_groups = 2
-mean_calculator.number_of_runs = 10
-mean_calculator.clustering_function = "kmeans2"
+mean_calculator = get_standard_mean_calculator(multithreading = True,
+        caching = False,
+        threshold = 1e-09,
+        number_of_groups = 4,
+        number_of_sub_groups = 4,
+        number_of_runs = 20,
+        clustering_function = 'kmeans2')
 
 means, labels = mean_calculator.calculate_means_data(lut_data_flatten)
+
+
 
 predicted = get_predicted_from_means(data = lut_data_flatten,
                                      means = means,
