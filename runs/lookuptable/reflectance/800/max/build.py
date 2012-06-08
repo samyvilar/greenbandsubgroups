c936e74deaf9d7a5f6c94aa57b0405d7b0f254d0
Samy Vilar <samy.vilar@gmail.com>
2012-06-08 19:08:43 -0400

    looking for max ...
    --HG--
    rename : runs/lookuptable/reflectance/800/min/build.py => runs/lookuptable/reflectance/800/max/build.py

--- runs/lookuptable/reflectance/800/max/build.py
+++ runs/lookuptable/reflectance/800/max/build.py
@@ -5,7 +5,7 @@ import sys
 import numpy
 sys.path.extend('../../../..')
 
-from lookuptable.lookuptable import build_lookuptable, lookuptable, update_min
+from lookuptable.lookuptable import build_lookuptable, lookuptable, update_max
 from Utils import get_all_granules_path, get_standard_granule_loader
 
 if __name__ == '__main__':
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     granule_loader_chunks = granule_loader.load_granules_chunk(dir = get_all_granules_path(), pattern = '*.hdf', chunks = chunk_size)
     lut_size = 800
     max_value = 1
-    mins = None
+    max = None
 
     for index, granule in enumerate(granule_loader_chunks):
         if granule:
@@ -23,27 +23,27 @@ if __name__ == '__main__':
                 new_lut = build_lookuptable({'data':granule[0].data,
                                              'size':lut_size,
                                              'max_value':max_value,
-                                             'function':'min'})
+                                             'function':'max'})
             except Exception as ex:
                 print str(ex)
                 continue
-            if mins == None:
-                mins = new_lut.min
+            if max == None:
+                max = new_lut.max
             else:
-                update_min(prev_mins = mins, new_mins = new_lut.min)
+                update_max(prev_max = max, new_max = max)
             del new_lut
             gc.collect()
         else:
             continue
 
-        mins[mins == (max_value + 1)] = numpy.nan
-        mins.tofile(str(lut_size) + '_min_lookuptable.numpy')
+        max[max == -1] = numpy.nan
+        max.tofile(str(lut_size) + '_max_lookuptable.numpy')
 
         lut = lookuptable()
-        lut.table = mins
+        lut.table = max
         lut.size = lut_size
         flatten_table = lut.flatten_2d_non_zero()
-        flatten_table.tofile(str(lut_size) + '_min_lookuptable_flatten.numpy')
+        flatten_table.tofile(str(lut_size) + '_max_lookuptable_flatten.numpy')
 
 '''
 2 mat files, one sorted by the (max - min) and the other by the standard deviation
