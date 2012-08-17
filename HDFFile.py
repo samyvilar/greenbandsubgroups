0885d9eb6c9c0cba314a95bf5ea4313b4c946854
Samy Vilar <samy.vilar@gmail.com>
2012-08-17 17:53:27 -0400

    minor bug ...

--- HDFFile.py
+++ HDFFile.py
@@ -12,7 +12,7 @@ def read_file(file = None, bands = None, param = None, crop_size = None, crop_or
     valid_range  = numpy.zeros((len(bands), 2))
 
     if param == 'reflectance':
-        temp_file = '/tmp/' + os.path.basename(file)
+        temp_file = '/tmp/temp_' + os.path.basename(file)
         shutil.copyfile(file, temp_file)
     elif param == 'radiance':
         temp_file = file
@@ -35,7 +35,7 @@ def read_file(file = None, bands = None, param = None, crop_size = None, crop_or
             b = granule.reflectance(band)
         elif param == 'radiance':
             b = granule.radiance(band)
-            
+
         data.append(b.read())
 
     if param == 'reflectance':
