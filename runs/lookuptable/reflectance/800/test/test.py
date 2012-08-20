fa917831b5c5f81efd292e7e0b190efcced9feb1
Samy Vilar <samy.vilar@gmail.com>
2012-08-20 01:22:11 -0400

    minor bugs ...

--- runs/lookuptable/reflectance/800/test/test.py
+++ runs/lookuptable/reflectance/800/test/test.py
@@ -12,18 +12,18 @@ if __name__ == '__main__':
     lut.load_table('../800_lookuptable.numpy')
 
     granule_path = '/home1/FermiData/DATA_3/SNOW_CLOUD_MODIS/data/MOD021KM.A2002179.1640.005.2010085164818.hdf'
-    original = read_file(file = granule_path,
+    original, valid_range, origina_shape = read_file(file = granule_path,
                          bands = [1,2,3,4],
                          param = 'reflectance',
                          winsize = 11,
                          maxinvalid = 0.4)
-    
+
     predicted = lut.predict(original)
 
     error = get_root_mean_square(original = original[:, 3], predicted = predicted[:, 3])
     print "RMSE: %f%%" % error
     print "Sum of Squared Errors: %f" % get_sum_of_errors_squared(original = original[:, 3], predicted = predicted[:, 3])
-    save_images(original = original, predicted = predicted, granule_path = granule_path, original_shape = granule_loader.granules[0].original_shape)
+    save_images(original = original, predicted = predicted, granule_path = granule_path, original_shape = origina_shape )
 
 
 # red is 1, green = 4, blue = 3, NIR = 2
